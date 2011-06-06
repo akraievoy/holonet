e4a5b6bf268855dcc30a0802f674e814d99ae73e
akraievoy <akraievoy@gmail.com>
2011-06-06 22:05:11 +0300

    Compilation error fixed, take 2

--- base.runner/src/main/java/org/akraievoy/base/runner/swing/ExperimentTableModel.java
+++ base.runner/src/main/java/org/akraievoy/base/runner/swing/ExperimentTableModel.java
@@ -30,6 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import javax.swing.table.AbstractTableModel;
 import java.sql.SQLException;
 import java.util.Collections;
