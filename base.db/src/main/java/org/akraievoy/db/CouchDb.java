b401edbac088955c1375821b12af95eeb7237712
AnriyLytvynov <lytvynov.a.v@gmail.com>
2011-11-13 11:31:55 +0200

    Added error logging for DB access attempt

--- base.db/src/main/java/org/akraievoy/db/CouchDb.java
+++ base.db/src/main/java/org/akraievoy/db/CouchDb.java
@@ -2,15 +2,18 @@ package org.akraievoy.db;
 
 import org.akraievoy.couch.CouchDao;
 import org.akraievoy.couch.Squab;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CouchDb {
+	private static final Logger log = LoggerFactory.getLogger(CouchDb.class);
 
 	protected CouchDao couch;
 
 	public CouchDb(CouchDao couch) {
 		this.couch = couch;
 	}
-	
+
 	public void start() {
 		checkConnection();
 	}
@@ -24,7 +27,11 @@ public class CouchDb {
 	}
 
 	private void checkConnection() {
-		couch.findAllPaths(Squab.class, "");  // FIXME Empty path parameter
+		try {
+			couch.findAllPaths(Squab.class, ""); // FIXME Empty path parameter
+		} catch (Exception e) {
+			log.error("Connection to couchDB failed.");
+		}
 	}
 
 }
