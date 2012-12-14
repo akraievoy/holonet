d43c0ac5fa45b1b182d92aebad1d6f30592a693f
akraievoy <akraievoy@gmail.com>
2012-12-14 19:12:40 +0200

    project is now monolithic single-module jar package [follow-up, compile-run-iterate sequence fixed]

--- iterate.sh
+++ iterate.sh
@@ -2,17 +2,12 @@
 set -o nounset
 set -o errexit
 
-mvn clean package
-mkdir $HOME/holonet.release-1.3-standalone/ | echo err status suppressed
-mv holonet.release/target/holonet.release-1.3-standalone.zip $HOME/holonet.release-1.3-standalone/
-cd $HOME/holonet.release-1.3-standalone/
-rm -rfv data logs
-7z x -y holonet.release-1.3-standalone.zip
-chmod u+x *.sh
-if [ -r ~/holonet.release-1.3-standalone.7z ]; then
+mvn clean test
+
+if [ -r data.7z ]; then
   rm -rf data/h2
-  7z x ~/holonet.release-1.3-standalone.7z -o.. -i!holonet.release-1.3-standalone/data/h2/
+  7z x data.7z
   echo '---done reloading old database---'
 fi
-./holonet.runner.sh
-cd $HOME/working/holonet.git
+
+mvn exec:java
