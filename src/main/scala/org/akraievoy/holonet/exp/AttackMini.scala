61f57ee4fb6e12d157988503eaedc3b2d3f9ab96
akraievoy <akraievoy@gmail.com>
2013-06-26 00:04:13 +0300

    attack-mini --- spotted error in link structure init

--- src/main/scala/org/akraievoy/holonet/exp/AttackMini.scala
+++ src/main/scala/org/akraievoy/holonet/exp/AttackMini.scala
@@ -27,12 +27,13 @@ import org.akraievoy.cnet.net.Net.{toId, toFrom, toInto, eClone}
 import org.akraievoy.cnet.metrics.domain._
 import org.akraievoy.cnet.metrics.api.Metric
 import org.akraievoy.holonet.exp.store.RefObject
-import org.akraievoy.cnet.net.vo.EdgeData.EdgeVisitor
+import java.util
+import gnu.trove.TIntArrayList
 
 object AttackMini extends App {
   val locRng: Random = new Random(123456L)
-  val linkRngSeed = 234567L
-  val attackRngSeed = 234567L
+  val linkRngSeed = 345678L
+  val attackRngSeed = 456789L
 
   val size: Int = 256
   val attackFraction: Double = .5
@@ -40,6 +41,7 @@ object AttackMini extends App {
 
   val distExpRange = -3.0 to 3.0 by 1
   val powExpRange = -3.0 to 3.0 by 1
+  val linkStructNum = 8
   val attackNum = 8
   val attackSize = floor(size * attackFraction).toInt
 
@@ -68,40 +70,49 @@ object AttackMini extends App {
   println("distExp;powExp;lambdaInit;effInit;connInit;attFrac;lambdaAtt;effAtt;connAtt")
   for (distExp <- distExpRange; powExp <- powExpRange) {
     val linkESource = new EntropySourceRandom().seed(linkRngSeed)
+    for (linkStruct <- 0 until linkStructNum) {
+      initLinks(powExp, distExp, linkESource)
 
-    initLinks(powExp, distExp, linkESource)
+      val lambdaInit = Metric.fetch(lambdaMetric.configure(linksRef))
+      val routeLen = Metric.fetch(routeLenMetric)
+      val effInit = Metric.fetch(effMetric.configure(new RefObject(routeLen), new RefObject[EdgeData](null)))
+      val connInit = Metric.fetch(connMetric.configure(new RefObject(routeLen), false))
 
-    val lambdaInit = Metric.fetch(lambdaMetric.configure(linksRef))
-    val routeLen = Metric.fetch(routeLenMetric)
-    val effInit = Metric.fetch(effMetric.configure(new RefObject(routeLen), new RefObject[EdgeData](null)))
-    val connInit = Metric.fetch(connMetric.configure(new RefObject(routeLen), false))
-
-    for (a <- 0 until attackNum) {
-      performAttack(a, eClone(links)){
-        case (linksAttack, lambdaIndex, attFrac) =>
-          val linksCompactRef = new RefObject(compact(linksAttack, lambdaIndex))
-          val distCompactRef = new RefObject(compact(dist, lambdaIndex))
-          val lambdaAttack = Metric.fetch(lambdaMetric.configure(linksCompactRef))
-          val routeLenMetricAttack = new MetricEDataRouteLen(
-            new MetricRoutesJohnson().configure(linksCompactRef, distCompactRef)
-          )
-          val routeLenAttack = Metric.fetch(routeLenMetricAttack)
-          val effAttack = Metric.fetch(
-            effMetric.configure(
-              new RefObject(routeLenAttack),
-              new RefObject[EdgeData](null)
+      for (a <- 0 until attackNum) {
+        performAttack(a, eClone(links)) {
+          case (linksAttack, lambdaIndex, attFrac) =>
+            val linksCompactRef = new RefObject(compact(linksAttack, lambdaIndex))
+            val distCompactRef = new RefObject(compact(dist, lambdaIndex))
+            val lambdaAttack = Metric.fetch(lambdaMetric.configure(linksCompactRef))
+            val routeLenMetricAttack = new MetricEDataRouteLen(
+              new MetricRoutesJohnson().configure(linksCompactRef, distCompactRef)
             )
-          )
-          val connAttack = Metric.fetch(connMetric.configure(new RefObject(routeLenAttack), false))
+            val routeLenAttack = Metric.fetch(routeLenMetricAttack)
+            val effAttack = Metric.fetch(
+              effMetric.configure(
+                new RefObject(routeLenAttack),
+                new RefObject[EdgeData](null)
+              )
+            )
+            val connAttack = Metric.fetch(connMetric.configure(new RefObject(routeLenAttack), false))
 
-          println(
-            Seq(distExp, powExp, lambdaInit, effInit, connInit, attFrac, lambdaAttack, effAttack, connAttack).mkString(";")
-          )
+            println(
+              Seq(
+                distExp,
+                powExp,
+                lambdaInit,
+                effInit,
+                connInit,
+                attFrac,
+                lambdaAttack,
+                effAttack,
+                connAttack
+              ).mkString(";")
+            )
+        }
       }
     }
   }
-
-
 
   def compact(e: EdgeData, index: Seq[Int]): EdgeData = {
     val eCompact = EdgeDataFactory.sparse(true, 0, index.size)
@@ -111,7 +122,7 @@ object AttackMini extends App {
     eCompact
   }
 
-  def performAttack(attackNum: Int, linksAttack: EdgeData)(reportFun: (EdgeData, Seq[Int], Double) => Unit): Unit = {
+  def performAttack(attackNum: Int, linksAttack: EdgeData)(reportFun: (EdgeData, Seq[Int], Double) => Unit) {
     val attackModel = new WeightedEventModelBase(Optional.of("attackModel"))
     val attackESource = new EntropySourceRandom().seed(attackRngSeed * 31 + attackNum)
     var lambdaIndex: Seq[Int] = range.toSeq
@@ -139,12 +150,17 @@ object AttackMini extends App {
     }
   }
 
-  def initLinks(powExp: Double, distExp: Double, linkESource: EntropySourceRandom): Any = {
+  def initLinks(powExp: Double, distExp: Double, linkESource: EntropySourceRandom) {
     links.clear()
     val powers: Array[Int] = range.map(i => 0).toArray
+    val connCache0 = new TIntArrayList()
     do {
       linkModel.clear()
-      for (f <- range; t <- range if f < t && powers(f) < linksPerNode && powers(t) < linksPerNode) {
+      for (
+        f <- range if powers(f) < linksPerNode;
+        connCache <- Some({ connCache0.clear(); links.connVertexes(f, connCache0) });
+        t <- f + 1 until size if powers(t) < linksPerNode && connCache.binarySearch(t) < 0
+      ) {
         linkModel.add(
           toId(f, t),
           pow(powers(f) + powers(t), powExp) + pow(dist.get(f, t), distExp)
@@ -152,7 +168,7 @@ object AttackMini extends App {
       }
 
       if (linkModel.getSize > 0) {
-        val linkId = linkModel.generate(linkESource, true, null)
+        val linkId = linkModel.generate(linkESource, false, null)
         val linkF = toFrom(linkId)
         val linkT = toInto(linkId)
         links.set(linkF, linkT, 1)
@@ -162,4 +178,5 @@ object AttackMini extends App {
 
     } while (linkModel.getSize > 0)
   }
+
 }
